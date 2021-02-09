14b65a6624bc25eebb5677a369b753a1aa4a4864
Adrien Dion <adion81@gmail.com>
2021-02-09 17:12:13 -0600

    initial commit

---
+++ notes.js
@@ -0,0 +1,82 @@
+// !Using VsCode 
+// !Extension: Better Comments for colorized comments.
+
+/**
+    * ! Getting Started with JavaScript
+    
+    * TODO: Primitive Variables in JS
+        * * Integers
+            *? Any whole number
+            @example var num = 1;
+        
+        * * Strings
+            *? A series of alpha-numeric characters wrapped in single or double  quotes.
+            @example var name = "Mr. Nibbles";
+            
+        * * Boolean
+            *? Either true(numerical rep of 1) or false(numerical rep of 0)
+            @example var isJavaScriptFun = true;
+    
+    * TODO: Basic Data Sets in JS
+        * * Arrays
+            *? A collection of variables that are indexed from 0 to the end of the array.
+            @example var arr = [1,2,3,4,5];
+
+        * * Objects
+            * ? A complex data set where we can hold multiple variables that relate to each other.
+            @example var obj = {
+                firstName: "Adrien",
+                lastName: "Dion",
+                age: 39
+            }
+    * TODO: Scoping and Hoisting
+        * * 3 Levels of Scope
+            * ? Global
+            * ? Functional
+            * ? Block
+        
+        * * Variables declared with the var keyword get hoisted up to the top of their scope
+        
+        * * If they are outside of a function, then they are global, if they are inside a function then they are functional.
+
+        * * Variables declared with let and const are not hoisted.
+
+    * TODO: Conditionals & Operators
+        * * List of Math Operators
+            * ?  + Addition
+            * ?  - Subtraction
+            * ?  * Multiplication
+            * ?  / Division
+            * ?  % Modulus
+        
+        * * List of Condiational Operators
+            * ?  == Comparison of variable values only.
+            * ?  === Comparison of variable value and type.
+            * ?  <=  Less than or Equal to.
+            * ?  >=  Greater than or Equal to.
+            * ?  !  Not - or 'falsey'
+            * ?  || Or statement
+            * ?  && And statement
+    * TODO: Functions
+        * * Functions are blocks of reusable code.
+        * * We define them as so
+        * @example function add(num1, num2){
+        *     return num1 + num2;
+        * }
+        *  * Return statements always end a function.
+        *  * We can call the function as so
+        *  @example add(2,3);
+        * 
+        * * We can also write them as arrow functions
+        * 
+        * @example const add = (num1,num2) => return num1 + num2;
+        * 
+        * * We would call it in the same way as a normal function.
+
+    * TODO: DOM Manipulation
+        * * JavaScript interperets the HTML file in an object form, hence the name Document Object Model.
+        * * We can write functions within our script tags and set them to be called on html tags with event listeners.
+        * * We can target elements on the page and assign them to variables.
+        * @example var h1 = document.getElementById("banana");
+*/
+
